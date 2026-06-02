bf2a0155c7a1b75625f9c53425e2d8a1480d0761
RodrigoSobrinhoPotter <94565771+RodrigoSobrinhoPotter@users.noreply.github.com>
2026-06-01 22:54:08 -0300

    feat(frontend): set default theme to dark mode

--- frontend/bravito_app/lib/app/theme_provider.dart
+++ frontend/bravito_app/lib/app/theme_provider.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ThemeModeNotifier extends Notifier<ThemeMode> {
   @override
-  ThemeMode build() => ThemeMode.light;
+  ThemeMode build() => ThemeMode.dark;
 
   void toggle() {
     state = state == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
